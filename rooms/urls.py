f5e28f3d71b93723b91e12332e5f0cee940a67ac
Alexis Belles Marza <abelles2071@gmail.com>
2016-10-12 20:30:37 +0200

    List my rooms

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-from rooms.views import CreateRoomView
+from rooms.views import RoomCreateView, MyRoomsListView
 
 urlpatterns = [
 
-    url(r'$', CreateRoomView.as_view(), name='room_create'),
+    url(r'create/$', RoomCreateView.as_view(), name='room_create'),
+    url(r'myrooms/$', MyRoomsListView.as_view(), name='myrooms_list'),
 
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,26 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.views.generic import CreateView
+from django.views.generic import ListView
 
-from forrent.settings import HOSTS_GROUP_NAME
 from rooms.forms import CreateRoomForm
+from rooms.models import Room
 
 
-class CreateRoomView(PermissionRequiredMixin, CreateView):
+class MyRoomsListView(LoginRequiredMixin, ListView):
+    template_name = 'myrooms_list.html'
+    paginate_by = 12
+
+    def get_queryset(self):
+        return Room.objects.filter(host=self.request.user).order_by('-available_since').select_related('host')
+
+
+class RoomCreateView(PermissionRequiredMixin, CreateView):
     form_class = CreateRoomForm
     template_name = 'room_create.html'
     success_url = '/'
-
-    def has_permission(self):
-        if not self.request.user.is_authenticated:
-            return False
-        if self.request.user.is_superuser:
-            return True
-        return HOSTS_GROUP_NAME in self.request.user.groups.values_list('name', flat=True)
+    permission_required = ('rooms.add_room',)
 
     def form_valid(self, form):
         room = form.save(commit=False)
         room.host = self.request.user
-        return super(CreateRoomView, self).form_valid(form)
+        return super(RoomCreateView, self).form_valid(form)
