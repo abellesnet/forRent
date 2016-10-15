30c59d347b20a65b430febcb9b1b91b8fe36611b
Alexis Belles Marza <abelles2071@gmail.com>
2016-10-15 19:16:23 +0200

    MyRoom detail

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-from rooms.views import RoomCreateView, MyRoomsListView
+from rooms.views import RoomCreateView, MyRoomsListView, MyRoomsDetailView
 
 urlpatterns = [
 
     url(r'create/$', RoomCreateView.as_view(), name='room_create'),
     url(r'myrooms/$', MyRoomsListView.as_view(), name='myrooms_list'),
+    url(r'myrooms/(?P<pk>[0-9]+)/$', MyRoomsDetailView.as_view(), name='myrooms_detail'),
 
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,10 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.views.generic import CreateView
+from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from rooms.forms import CreateRoomForm
 from rooms.models import Room
-
-
-class MyRoomsListView(LoginRequiredMixin, ListView):
-    template_name = 'myrooms_list.html'
-    paginate_by = 12
-
-    def get_queryset(self):
-        return Room.objects.filter(host=self.request.user).order_by('-available_since').select_related('host')
 
 
 class RoomCreateView(PermissionRequiredMixin, CreateView):
@@ -24,3 +17,22 @@ class RoomCreateView(PermissionRequiredMixin, CreateView):
         room = form.save(commit=False)
         room.host = self.request.user
         return super(RoomCreateView, self).form_valid(form)
+
+
+class MyRoomsListView(LoginRequiredMixin, ListView):
+    template_name = 'myrooms_list.html'
+    paginate_by = 12
+
+    def get_queryset(self):
+        return Room.objects.filter(host=self.request.user).order_by('-available_since').select_related('host')
+
+
+class MyRoomsDetailView(PermissionRequiredMixin, DetailView):
+    model = Room
+    template_name = 'myrooms_detail.html'
+
+    def has_permission(self):
+        room = Room.objects.filter(pk=self.kwargs.get('pk')).first()
+        if room and room.host == self.request.user:
+            return True
+        return False
