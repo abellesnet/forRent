43505c425afc9b0cc0c108d1249f94478718aefc
Alexis Belles Marza <abelles2071@gmail.com>
2016-11-26 17:49:06 +0100

    Translate the site to spanish (es)

--- forrent/users/forms.py
+++ forrent/users/forms.py
@@ -3,11 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.forms import CharField
+from django.utils.translation import ugettext_lazy as _
 
 
 class CreateUserForm(UserCreationForm):
-    first_name = CharField(max_length=30, min_length=1)
-    last_name = CharField(max_length=30, min_length=1)
+    first_name = CharField(label=_('First name'), max_length=30, min_length=1)
+    last_name = CharField(label=_('Last name'), max_length=30, min_length=1)
     email = CharField(min_length=1)
 
     # TODO: improve groups widget in register form
@@ -19,13 +20,13 @@ class CreateUserForm(UserCreationForm):
         email = self.cleaned_data.get('email')
         user_with_same_email = User.objects.filter(email=email)
         if user_with_same_email.count() > 0:
-            raise ValidationError('A user with that email already exists.')
+            raise ValidationError(_('A user with that email already exists.'))
         return email
 
     def clean_groups(self):
         groups = self.cleaned_data.get('groups')
         if len(groups) != 1:
-            raise ValidationError('You must select one group.')
+            raise ValidationError(_('You must select one group.'))
         return groups
 
     def save(self, commit=True):
