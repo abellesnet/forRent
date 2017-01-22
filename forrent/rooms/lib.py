c33f10fe8eeaf99430929297fd43681d02fff6e1
Alexis Belles Marza <abelles2071@gmail.com>
2017-01-22 11:47:58 +0100

    Refactor some tests

--- forrent/rooms/lib.py
+++ forrent/rooms/lib.py
@@ -8,17 +8,18 @@ from pika import ConnectionParameters
 
 
 def generate_responsive_room_main_photo_images(image):
-    logging.getLogger('forrent').info('Sending image {0} to {1}'.format(image.name, settings.BROKER_HOST))
-    connection = BlockingConnection(ConnectionParameters(settings.BROKER_HOST))
-    channel = connection.channel()
-    channel.queue_declare(queue=settings.RESPONSIVE_IMAGE_QUEUE, durable=True)
-    channel.basic_publish(
-        exchange='',
-        routing_key=settings.RESPONSIVE_IMAGE_QUEUE,
-        body=image.name,
-        properties=BasicProperties(
-            delivery_mode=2,  # make message persistent
-        ),
-    )
-    logging.getLogger('forrent').info('Sent image {0} to {1}'.format(image.name, settings.BROKER_HOST))
-    connection.close()
+    if image:
+        logging.getLogger('forrent').info('Sending image {0} to {1}'.format(image.name, settings.BROKER_HOST))
+        connection = BlockingConnection(ConnectionParameters(settings.BROKER_HOST))
+        channel = connection.channel()
+        channel.queue_declare(queue=settings.RESPONSIVE_IMAGE_QUEUE, durable=True)
+        channel.basic_publish(
+            exchange='',
+            routing_key=settings.RESPONSIVE_IMAGE_QUEUE,
+            body=image.name,
+            properties=BasicProperties(
+                delivery_mode=2,  # make message persistent
+            ),
+        )
+        logging.getLogger('forrent').info('Sent image {0} to {1}'.format(image.name, settings.BROKER_HOST))
+        connection.close()
